d5a2c5eb7d1686163ad33f8b331d15d496b25f30
pogikeith <pogikeith@yahoo.com>
2019-10-25 14:06:23 -0600

    fixed things

--- src/components/Pages/home.js
+++ src/components/Pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import PortfolioContainer from "../portfolio/portfolio-container";
+import PortfolioContainer from "../Portfolio/portfolio-container";
 
 export default function() {
   return (
